df16cf58c9bb7b1a3fd020f5e3e1c1e239733c25
welschoff <h.welschoff@gmx.de>
2021-11-24 12:04:59 +0100

    :heavy_plus_sign: Add mongodb and dotenv

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,43 @@
 import express from 'express';
-import path from 'path';
+// import path from 'path';
+import { connectDatabase } from './database';
+import { getUserCollection } from './database';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const port = process.env.PORT || 3001;
 const app = express();
 
-app.get('/api/hello', (_request, response) => {
-  response.json({ message: 'Hello from server' });
+if (!process.env.MONGODB_URI) {
+  throw new Error('No MongoDB URI dotenv variable');
+}
+
+connectDatabase(process.env.MONGODB_URI).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
+
+// GET all users
+app.get('/api/users/', async (_request, response) => {
+  const usersCollection = getUserCollection();
+  const currywurst = usersCollection.find();
+  const allUsers = await currywurst.toArray();
+  response.send(allUsers);
 });
 
-app.get('/', (_request, response) => {
-  response.send('We are on home');
+app.get('/api/hello', (_request, response) => {
+  response.json({ message: 'Hello from server' });
 });
 
 // Serve production bundle
 app.use(express.static('dist'));
 
 // Handle client routing, return all requests to the app
-app.get('*', (_request, response) => {
-  response.sendFile(path.join(__dirname, '../dist/index.html'));
-});
+// app.get('*', (_request, response) => {
+//   response.sendFile(path.join(__dirname, '../dist/index.html'));
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
