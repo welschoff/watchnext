46e6ebbda69bdacacdb1771966d305c9d821c77e
welschoff <h.welschoff@gmx.de>
2021-11-26 13:56:44 +0100

    :recycle: Refactor code

---
+++ src/pages/Search/Search.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import SearchResult, {
+  SearchResultProps,
+} from '../../components/SearchResult/SearchResult';
+import styles from './Search.module.css';
+import OverlayMenu from '../../components/OverlayMenu/OverlayMenu';
+
+function Search(): JSX.Element {
+  const [results, setResults] = useState<SearchResultProps[] | null>([]);
+  const [query, setQuery] = useState<string | ''>('');
+
+  const getSeries = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/tv?api_key=${
+        import.meta.env.API_KEY
+      }&query=${query}`
+    );
+    const data = await response.json();
+    console.log(data);
+    setResults(data.results);
+  };
+
+  useEffect(() => {
+    getSeries();
+  }, [query]);
+
+  return (
+    <div className={styles.container}>
+      <OverlayMenu />
+      <form className={styles.search}>
+        <input
+          type="search"
+          placeholder="Search Series"
+          value={query}
+          onChange={(event) => {
+            setQuery(event.target.value);
+          }}
+        />
+      </form>
+      <div>
+        {results?.map((result) => (
+          // eslint-disable-next-line react/jsx-key
+          <SearchResult
+            poster_path={result.poster_path}
+            name={result.name}
+            vote_average={result.vote_average}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Search;
