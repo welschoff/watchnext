2d1794948a7428c5df023b56f89d35f7dbdf5e4d
welschoff <h.welschoff@gmx.de>
2021-12-06 17:51:54 +0100

    :sparkles: Prepare Watchlist

---
+++ src/pages/Watchlist/Watchlist.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import WatchlistCard from '../../components/WatchlistCard/WatchlistCard';
+import { WatchlistCardProps } from '../../components/WatchlistCard/WatchlistCard';
+
+function Watchlist() {
+  const [series, setSeries] = useState<WatchlistCardProps | null>(null);
+
+  const getDetails = async () => {
+    const response = await fetch('/api/detail/90462');
+    const data = await response.json();
+    setSeries(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getDetails();
+  }, []);
+
+  return (
+    <div>
+      {series && (
+        <WatchlistCard
+          poster_path={series.poster_path}
+          vote_average={series.vote_average}
+          number_of_seasons={series.number_of_seasons}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Watchlist;
