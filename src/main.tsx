320999f4f4f3103e2b0924e3c4dc2dbf31d0a674
welschoff <h.welschoff@gmx.de>
2021-12-16 20:54:23 +0100

    :bug: Wrong import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './global.css';
+import './globals.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
