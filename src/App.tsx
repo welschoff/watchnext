9052bca4329f623c9653d26f97d1c0c92baf4187
welschoff <h.welschoff@gmx.de>
2022-01-04 16:24:53 +0100

    :sparkles: Add Page transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Detail from './pages/Detail/Detail';
 import FriendsPage from './pages/FriendsPage/FriendsPage';
 import FriendsWatchlist from './pages/FriendsWatchlist/FriendsWatchlist';
@@ -6,11 +6,20 @@ import Login from './pages/LoginPage/Login';
 import Popular from './pages/Popular/Popular';
 import Search from './pages/Search/Search';
 import Watchlist from './pages/Watchlist/Watchlist';
+import { useTransition, animated } from 'react-spring';
 
 function App() {
-  return (
-    <div>
-      <Routes>
+  const location = useLocation();
+
+  const transitions = useTransition(location, {
+    from: { transform: 'translateX(100%)' },
+    enter: { transform: 'translateX(0)' },
+    leave: { transform: 'translateX(-100%)' },
+  });
+
+  return transitions((props, item) => (
+    <animated.div style={props}>
+      <Routes location={item}>
         <Route path="/" element={<Login />} />
         <Route path="/popular" element={<Popular />} />
         <Route path="/popular/:id" element={<Detail />} />
@@ -22,8 +31,8 @@ function App() {
         <Route path="/friends/watchlist" element={<FriendsWatchlist />} />
         <Route path="/friends/watchlist/:id" element={<Detail />} />
       </Routes>
-    </div>
-  );
+    </animated.div>
+  ));
 }
 
 export default App;
